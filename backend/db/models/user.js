6e20e8ee5717e1f2050c3d9e516c6664f6778971
xiaoqin001 <xiaoqin6787@outlook.com>
2023-02-03 17:05:32 -0500

    added model scope and model method

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 const { Validator } = require('sequelize');
+const { bcrypt } = require('bcryptjs');
+const { findDOMNode } = require('react-dom');
 
 
 module.exports = (sequelize, DataTypes) => {
@@ -33,9 +35,70 @@ module.exports = (sequelize, DataTypes) => {
         len: [60, 60]
       }
     },
-  }, {});
+  }, {
+    defualtScope: {
+      attributes:{
+        exclude: ['hashedPassword', 'updatedAt', 'email', 'createdAt']
+      }
+    },
+    scope: {
+      currentUser: {
+        attributes:{
+          exclude: ['hashedPassword']
+        }
+      },
+      loginUser: {
+        attributes:{}
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
   return User;
 };
+
+// Authentication Flow
+// User Model Methods
+
+// instance method
+User.prototype.toSafeObject = function() {
+  const {id, username, email} = this;
+  return {id, username, email};
+}
+
+
+User.prototype.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.hashedPassword.toString());
+}
+
+// static method
+User.getCurrentUserById = async function(id) {
+  return await User.scope('currentUser').findByPk(id);
+}
+
+User.login = async function({ credential, password }) {
+  const Op = require('sequelize');
+  const user = User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential
+      }
+    }
+  }
+  )
+  if (user && User.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id)
+  }
+}
+
+User.signup = async function({ username, email, password }) {
+  const hashedPassword = bcrypt.hashedPassword(password)
+  const user = User.create({
+    username,
+    email,
+    hashedPassword
+  })
+  return await User.scope('currentUser').findBypk(user.id)
+}
