ae222755951da5386eb87c4307cb55ff78580d52
xiaoqin001 <xiaoqin6787@outlook.com>
2023-02-08 18:17:20 -0500

    navigation and profile button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from './store/session';
 
+
+// function App() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <Navigation isLoaded={isLoaded} />
+//       {isLoaded && (
+//         <Switch>
+//           <Route path="/signup">
+//             <SignupFormPage />
+//           </Route>
+//         </Switch>
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
 
 
 function App() {
@@ -14,17 +38,21 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(()=>setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
+  return (
   // return (
-    <Switch>
-      <Route path='/login'>
-        <LoginFormPage />
-      </Route>
-      <Route path='/signup'>
-        <SignupFormPage />
-      </Route>
-    </Switch>
-
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path='/login'>
+            <LoginFormPage />
+          </Route>
+          <Route path='/signup'>
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import ProfileButton from "./ProfileButton";
+import './Navigation.css';
+
+function Navigation({ isLoaded }) {
+    const sessionUser = useSelector(state=>state.session.user);
+    let sessionLinks;
+
+    if (sessionUser) {
+        sessionLinks = (
+            <ProfileButton user={sessionUser} />
+        );
+    } else {
+        sessionLinks = (
+            <>
+                <NavLink to='/login'>Log In</NavLink>
+                <NavLink to='/signup'>Sign Up</NavLink>
+            </>
+        );
+    }
+    console.log(sessionLinks)
+    return (
+        <ul>
+            <li>
+                <NavLink exact to='/'>Home</NavLink>
+                {isLoaded && sessionLinks}
+            </li>
+        </ul>
+    )
+}
+
+
+
+export default Navigation;
+
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import LoginFormModal from '../LoginFormModal';
+// import './Navigation.css';
+
+// function Navigation({ isLoaded }){
+//   const sessionUser = useSelector(state => state.session.user);
+
+//   let sessionLinks;
+//   if (sessionUser) {
+//     sessionLinks = (
+//       <ProfileButton user={sessionUser} />
+//     );
+//   } else {
+//     sessionLinks = (
+//       <>
+//         <LoginFormModal />
+//         <NavLink to="/signup">Sign Up</NavLink>
+//       </>
+//     );
+//   }
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink exact to="/">Home</NavLink>
+//         {isLoaded && sessionLinks}
+//       </li>
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
